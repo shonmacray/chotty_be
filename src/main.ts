6eac2a65f1af90b20c362b8cf07b92995a61362a
shonmacray <shonmacray@gmail.com>
2024-08-06 12:16:59 +0300

    bind port for render

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({ origin: ['http://localhost:3000'] });
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(8000);
+  const port = process.env.PORT || 8000;
+  await app.listen(port);
 }
 bootstrap();
