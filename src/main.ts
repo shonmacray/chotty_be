a283cdb6a9c7ce68eafb10f2b79b5c916f5afc78
shonmacray <shonmacray@gmail.com>
2024-08-06 12:37:19 +0300

    added prod url for cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: ['http://localhost:3000'] });
+  app.enableCors({
+    origin: ['http://localhost:3000', 'https://chotty-fe.vercel.app'],
+  });
   app.useGlobalPipes(new ValidationPipe());
   const port = process.env.PORT || 8000;
   await app.listen(port);
